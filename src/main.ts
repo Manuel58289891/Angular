4aa33d9406caed26ef14d597aaca72e21b1614ea
Manuel58289891 <rodriguezmanuelalejandro2001@gmail.com>
2025-10-12 16:01:17 -0400

    json server

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { LoginRegistrerComponent } from './app/login-registrer/login-registrer.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-bootstrapApplication(LoginRegistrerComponent, {
-  providers: [
-    provideRouter(routes)
-  ]
-}).catch(err => console.error(err));
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
-
-  bootstrapApplication(AppComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
-    provideAnimations() // <--- esto reemplaza BrowserAnimationsModule
-  ]
-});
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideAnimations(),
+  ],
+}).catch((err) => console.error(err));
